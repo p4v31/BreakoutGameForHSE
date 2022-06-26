a1600f2fbca0d0269a9403f873387c67f40c344f
p4v31 <90404785+p4v31@users.noreply.github.com>
2022-06-26 21:13:03 +0300

    Update test.h

--- cloudout/Headers/test.h
+++ cloudout/Headers/test.h
@@ -13,35 +13,73 @@ private:
     {
         return true;
     }
+
     bool gameWon()
     {
         return false;
     }
+
     bool gameOver()
     {
         return false;
     }
+
     bool gameStarted()
     {
         return false;
     }
+
     bool paused()
     {
         return false;
     }
+
 private slots:
     void initTestCase()
     {
         qDebug("Test is on");
     }
+
     void firstTest()
     {
         QVERIFY(true);
-        QCOMPARE(1,1);
+
     }
+
+    void secondTest(){
+
+        QVERIFY(false);
+
+    }
+
+    void thirdTest(){
+
+        QCOMPARE(1,1);
+
+    }
+
+    void fourthTest(){
+
+        QCOMPARE(1,1);
+
+    }
+
+    void fifthTest(){
+
+        QCOMPARE(1,10);
+
+    }
+
+    void sixthTest(){
+
+        QCOMPARE(0,0);
+
+    }
+
     void cleanupTestCase()
     {
         qDebug("end");
     }
+
 };
 #endif // TEST_H
